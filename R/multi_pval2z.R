9637dd2e7a7bc25d12720222f57d09282fd72c95
Pan <PanH@joslin.harvard.edu>
2019-09-30 12:53:05 -0400

    add multi_pval2z

---
+++ R/multi_pval2z.R
@@ -0,0 +1,29 @@
+#' Convert the table of p-values to Z-scores
+#'
+#' Convert the table of two-sided test p-values to Z-scores
+#' @param p.suffix The suffix of p-value columns
+#' @param direction.suffix The suffix ofdirection columns such as log fold-change, regression slope, correlation coefficient, or the"Up/Down" valuses.
+#' @inheritParams ezlimmaplot::ezvenn
+#' @export
+
+multi_pval2z <- function(tab, prefix.v=NULL, p.suffix="p", direction.suffix="logFC"){
+
+  stopifnot(length(p.suffix)==1, length(direction.suffix)==1)
+
+  if(!is.null(prefix.v)){
+    p.cols <- paste(prefix.v, p.suffix, sep = ".")
+    stopifnot(p.cols %in% colnames(tab))
+
+  }else{
+    p.suffix <- paste0("\\.", p.suffix)
+    p.cols <- grep(p.suffix, colnames(tab), value = TRUE)
+    prefix.v <- gsub(p.suffix, "", p.cols)
+  }
+
+  direction.cols <- paste(prefix.v, direction.suffix, sep = ".")
+  stopifnot(direction.cols %in% colnames(tab))
+
+  res <- vapply(seq_along(prefix.v), FUN=function(i) {pval2z(tab[,p.cols[i]], direction=tab[,direction.cols[i]])}, FUN.VALUE=numeric(nrow(tab)))
+  if(length(prefix.v) > 1) dimnames(res) <-  list(rownames(tab), prefix.v)
+  return(res)
+}
---
+++ tests/testthat/test-multi_pval2z.R
@@ -0,0 +1,26 @@
+context("multi_pval2z")
+
+mtt <- ezlimma::limma_contrasts(M, grp=grp, contrast.v = contr.v)
+mtt1 <- mtt
+mtt1$Middle3vsFirst3.Direction <- ifelse(mtt1$Middle3vsFirst3.logFC>0, "Up", "Down")
+mtt1$Last3vsFirst3.Direction <- ifelse(mtt1$Last3vsFirst3.logFC>0, "Up", "Down")
+
+test_that("only tab", {
+  res <- multi_pval2z(mtt)
+  expect_equal(as.vector(res[1, ]), c(7.972129881, 7.96543488))
+})
+
+test_that("with prefix.v", {
+  res <- multi_pval2z(mtt, prefix.v = names(contr.v))
+  expect_equal(as.vector(res[1, ]), c(7.972129881, 7.96543488))
+})
+
+test_that("one comparison", {
+  res <- multi_pval2z(mtt, prefix.v = names(contr.v)[1])
+  expect_equal(res[1:3], c(7.972129881, 1.426847696, 6.473577613))
+})
+
+test_that("with Up/Down", {
+  res <- multi_pval2z(mtt1, direction.suffix = "Direction")
+  expect_equal(as.vector(res[1, ]), c(7.972129881, 7.96543488))
+})
