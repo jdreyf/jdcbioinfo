45b8ed3e97999b6fa60808c30362ed9b7078233d
Pan <PanH@joslin.harvard.edu>
2019-10-02 10:26:22 -0400

    update test_correct_null_distribution

--- tests/testthat/test-correct_null_distribution.R
+++ tests/testthat/test-correct_null_distribution.R
@@ -1,13 +1,13 @@
 context("correct_null_distribution")
 
 test_that("without prefix.v", {
-  suppressWarnings(mtt <- correct_null_distribution(mtt))
-  expect_equal(mtt[1, "Middle3vsFirst3.p"], 1.559628e-15)
+  suppressWarnings(mtt1 <- correct_null_distribution(mtt))
+  expect_equal(mtt1[1, "Middle3vsFirst3.p"], 1.559628e-15)
 })
 
 test_that("with prefix.v", {
-  suppressWarnings(mtt <- correct_null_distribution(mtt, prefix.v = names(contr.v)))
-  expect_equal(mtt[1, "Middle3vsFirst3.p"], 1.559628e-15)
+  suppressWarnings(mtt1 <- correct_null_distribution(mtt, prefix.v = names(contr.v)))
+  expect_equal(mtt1[1, "Middle3vsFirst3.p"], 1.559628e-15)
 })
 
 
@@ -15,6 +15,6 @@ test_that("Direction input", {
   mtt_d <- mtt
   mtt_d$Middle3vsFirst3.Direction <- ifelse(mtt_d$Middle3vsFirst3.logFC>0, "Up", "Down")
   mtt_d$Last3vsFirst3.Direction <- ifelse(mtt_d$Last3vsFirst3.logFC>0, "Up", "Down")
-  suppressWarnings(mtt <- correct_null_distribution(mtt))
-  expect_equal(mtt[1, "Middle3vsFirst3.p"], 1.559628e-15)
+  suppressWarnings(mtt_d <- correct_null_distribution(mtt_d, direction.suffix="Direction"))
+  expect_equal(mtt_d[1, "Middle3vsFirst3.p"], 1.559628e-15)
 })
