7b5e4c04313e1106abf7ff39eaa32e89ad699bbe
Pan <PanH@joslin.harvard.edu>
2019-10-23 14:37:18 -0400

    add cor_by_grp & edger_contrasts

---
+++ tests/testthat/test-edger_contrasts.R
@@ -0,0 +1,22 @@
+context("edger_contrasts")
+
+test_that("edger contrasts", {
+
+  #default
+  res <- edger_contrasts(dge, grp=grp, contrast.v = contr.v)
+  expect_equal(rownames(res$mtt)[1:3], paste0("gene", c(3,1,2)))
+
+  #trend
+  suppressWarnings(res <- edger_contrasts(dge, grp=grp, contrast.v = contr.v, test="LRT"))
+  expect_equal(rownames(res$mtt)[1:3], paste0("gene", c(3,62,1)))
+
+  # design
+  res <- edger_contrasts(dge, grp=grp, contrast.v = contr.v, design = design)
+  expect_equal(rownames(res$mtt)[1:3], paste0("gene", 3:1))
+
+  # weights
+  dge1 <- dge
+  dge1$weights <- el$weights
+  suppressWarnings(res <- edger_contrasts(dge1, grp=grp, contrast.v = contr.v, test="WeightedFT"))
+  expect_equal(rownames(res$mtt)[1:3], paste0("gene", 1:3))
+})
