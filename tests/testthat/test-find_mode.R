c978e46e9ee00572e254538cbe01ce57b1ef62db
Pan <PanH@joslin.harvard.edu>
2019-12-19 16:55:03 -0500

    add find_mode

---
+++ tests/testthat/test-find_mode.R
@@ -0,0 +1,11 @@
+context("find_mode")
+
+test_that("find mode", {
+  x1 <- stats::rnorm(1000, mean=10, sd=2)
+  x2 <- stats::rnorm(1000, mean=20, sd=3)
+  x <- c(x1, x2)
+  mds <- find_mode(x, bin=30, plot=FALSE)
+  expect_equal(round(mds$mds.vals), c(10, 20))
+})
+
+
