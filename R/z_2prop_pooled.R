b1d98226878f353963e1680e3e73847a6faa578d
Hui Pan <Hui.Pan@joslin.harvard.edu>
2020-03-03 14:46:47 -0500

    add z_2prop_pooled

---
+++ R/z_2prop_pooled.R
@@ -0,0 +1,39 @@
+#' Two-proportion z-test (pooled) for non-zero null
+#'
+#' Two-proportion z-test (pooled) for non-zero null.
+#'
+#' @param x1 number of successes in group 1.
+#' @param x2 number of successes in group 2.
+#' @param n1 total number of games in group 1.
+#' @param n2 total number of games in group 2.
+#' @param delta null hypothesis of the difference in proportion.
+#' @inheritParams stats::prop.test
+#' @return a number of p-value.
+#' @export
+
+z_2prop_pooled <- function(x1, x2, n1, n2, delta=0, alternative=c("two.sided", "less", "greater")){
+
+  stopifnot(x1<=n1, x2<=n2, x1>=0, x2>=0, n1>0, n2>0)
+  alternative <- match.arg(alternative)
+
+  # get z-score
+  p.diff <- (x1/n1)-(x2/n2)-delta
+  p.common <- (x1+x2)/(n1+n2)
+  se <- sqrt(p.common * (1-p.common) * (1/n1+1/n2))
+  z.score <- p.diff/se
+
+  # get p-value
+  if (alternative=="two.sided") {
+    pval <- stats::pnorm(z.score, lower.tail=TRUE)
+    pval <- 2*min(pval, 1-pval)
+    if (pval>1) pval <- 1
+
+  } else if (alternative=="less") {
+    pval <- stats::pnorm(z.score, lower.tail=TRUE)
+
+  } else if (alternative=="greater") {
+    pval <- stats::pnorm(z.score, lower.tail=FALSE)
+  }
+
+  return(pval)
+}
