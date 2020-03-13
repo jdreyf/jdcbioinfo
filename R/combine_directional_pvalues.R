e13351974f614513321d3879f946c9ff54565503
jonathan dreyfuss <jdreyf@gmail.com>
2020-03-13 14:14:53 -0400

    Update combine_directional_pvalues.R

--- R/combine_directional_pvalues.R
+++ R/combine_directional_pvalues.R
@@ -10,19 +10,19 @@
 
 combine_directional_pvalues <- function(tab, prefix.v=NULL, p.suffix="p", direction.suffix="logFC", only.p=FALSE, prefix=NULL){
 
-  mat.z <- multi_pval2z(tab, prefix.v=prefix.v, p.suffix=p.suffix, direction.suffix=direction.suffix)
+  mat.z <- jdcbioinfo::multi_pval2z(tab, prefix.v=prefix.v, p.suffix=p.suffix, direction.suffix=direction.suffix)
   combz.v <- rowSums(mat.z)/sqrt(ncol(mat.z))
   combp.v <- stats::pnorm(combz.v)
   combp.v <- 2*pmin(combp.v, 1-combp.v)
 
   if(only.p){
-    combp.v <- setNames(combp.v, nm=rownames(tab))
+    combp.v <- stats::setNames(combp.v, nm=rownames(tab))
     return(combp.v)
   }
 
   direction <- ifelse(combz.v>0, "Up", "Down")
   fdr <- stats::p.adjust(combp.v, method="BH")
-  res <- data.frame(Combined.Direction=direction, Combined.p= combp.v, Combined.FDR=fdr)
+  res <- data.frame(Combined.Direction=direction, Combined.p=combp.v, Combined.FDR=fdr)
   if(!is.null(prefix)) colnames(res) <- paste(prefix, colnames(res), sep=".")
 
   return(res)
