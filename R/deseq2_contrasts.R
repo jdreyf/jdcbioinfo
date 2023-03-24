ee90b9b06dd249857148ce26707e884574aad0df
Pan <PanH@joslin.harvard.edu>
2023-03-24 16:29:24 -0400

    add shrunken choice to deseq2_countrasts

--- R/deseq2_contrasts.R
+++ R/deseq2_contrasts.R
@@ -3,11 +3,12 @@
 #' Apply \pkg{DESeq2}'s \code{DESeq}, \code{results},and \code{lfcShrink} to one or more contrasts, and return a data.frame
 #' @param dds DESeqDataSet object.
 #' @param ncore Number of cores to use.
+#' @param shrunken logical, whether to shrink the log fold-change
 #' @inheritParams ezlimma::limma_contrasts
 #' @details  \code{grp} isn't needed if \code{add.means} is \code{FALSE}.
 #' @export
 
-deseq2_contrasts <- function(dds, grp=NULL, contrast.v, add.means=!is.null(grp), cols=c("pvalue", "padj", "log2FoldChange"), ncore=1){
+deseq2_contrasts <- function(dds, grp=NULL, contrast.v, add.means=!is.null(grp), cols=c("pvalue", "padj", "log2FoldChange"), ncore=1, shrunken=TRUE){
 
     if (add.means) { stopifnot(ncol(dds)==length(grp), colnames(dds)==names(grp)) }
 
@@ -23,7 +24,12 @@ deseq2_contrasts <- function(dds, grp=NULL, contrast.v, add.means=!is.null(grp),
 
     for (i in 1:ncol(contr.mat)) {
       tt <- DESeq2::results(dds, contrast=contr.mat[, i], test="Wald", parallel=TRUE, BPPARAM=bp)
-      shrunkenTT <- DESeq2::lfcShrink(dds, contrast=contr.mat[, i], res=tt, type="ashr", parallel=TRUE, BPPARAM=bp)
+      if (shrunken) {
+        shrunkenTT <- DESeq2::lfcShrink(dds, contrast=contr.mat[, i], res=tt, type="ashr", parallel=TRUE, BPPARAM=bp)
+      } else {
+        shrunkenTT <- tt
+      }
+
       shrunkenTT <- as.data.frame(shrunkenTT)[, cols]
       colnames(shrunkenTT) <- gsub("pvalue", "p", gsub("padj", "FDR", gsub("log2FoldChange", "logFC", colnames(shrunkenTT))))
       shrunkenTT$FC <- sign(shrunkenTT$logFC)*2^abs(shrunkenTT$logFC)
