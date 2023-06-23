9b5988299c140395a669ffde124322b54b6a1ae4
Pan <PanH@joslin.harvard.edu>
2023-06-23 15:47:58 -0400

    update select_top_2sided

--- R/select_top_2sided.R
+++ R/select_top_2sided.R
@@ -9,17 +9,18 @@
 #' @return Vector of the top feature's IDs (the rownames of topTable).
 #' @export
 
-select_top <-  function(tab, prefix.v=NULL, p.suffix="p", direction.suffix="logFC", ntop=50, ntop.offset=5, each=TRUE){
+select_top_2sided <-  function(tab, prefix.v=NULL, p.suffix="p", direction.suffix="logFC", ntop=50, ntop.offset=5, each=TRUE){
 
   if(nrow(tab)<=ntop) return(rownames(tab))
 
   mat.z <- multi_pval2z(tab, prefix.v=prefix.v, p.suffix=p.suffix, direction.suffix=direction.suffix)
+  ntop.each <- ceiling(ntop/2)
 
   if(!each){
-    top <- rownames(tab)[order(ezlimma::combine_pvalues(tab[, p.cols]))][1:ntop]
+    sum.z <- rowSums(mat.z)
+    top <- union(rownames(mat.z)[order(-sum.z)][1:ntop.each], rownames(mat.z)[order(sum.z)][1:ntop.each])
   } else {
     ord <- list()
-    ntop.each <- ceiling(ntop/2)
     for (i in 1:ncol(mat.z)) {
       ord[[paste0("up", i)]] <- rownames(mat.z)[order(-mat.z[,i])][1:ntop.each]
       ord[[paste0("dn", i)]] <- rownames(mat.z)[order(mat.z[,i])][1:ntop.each]
