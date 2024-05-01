46388117cea7e9c8b47a5c159d75ef2d83228016
Pan <PanH@joslin.harvard.edu>
2024-05-01 14:39:38 -0400

    modify pval2z to accept NAs

--- R/pval2z.R
+++ R/pval2z.R
@@ -10,7 +10,7 @@
 
 pval2z <- function(pval, direction){
   stopifnot(length(pval)==length(direction), names(pval)==names(direction))
-  if(any(pval<0 | pval>1)) stop("p-values should be within [0, 1]")
+  if(any(pval<0 | pval>1, na.rm = TRUE)) stop("p-values should be within [0, 1]")
 
   if(is.numeric(direction)){
     direction <- sign(direction)
