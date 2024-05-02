1fc5a99e350d45934913ec88ccdbc828bdc03249
Pan <PanH@joslin.harvard.edu>
2024-05-02 08:37:26 -0400

    add resolution argument to make_gsea_cnet

--- R/make_gsea_cnet.R
+++ R/make_gsea_cnet.R
@@ -4,10 +4,12 @@
 #'
 #' @param gseaRes a named list of GSEA object
 #' @param fc a list of Z-scores with the same names as gseaRes
+#' @resolution Optional resolution parameter
+#' @label label for color bar
 #' @return a list of ggplot2 objects.
 #' @export
 
-make_gsea_cnet <- function(gseaRes, fc) {
+make_gsea_cnet <- function(gseaRes, fc, resolution = 1, label = "Z-scores") {
   gseaRes <- lapply(gseaRes, FUN = enrichplot::pairwise_termsim, method = "JC")
 
   pltList <- list()
@@ -17,7 +19,7 @@ make_gsea_cnet <- function(gseaRes, fc) {
     mat[is.na(mat)] <- 0
     net <- igraph::graph_from_adjacency_matrix(mat, mode = "max", weighted = TRUE)
 
-    cluster.out <- igraph::cluster_louvain(net)
+    cluster.out <- igraph::cluster_louvain(net, resolution = resolution)
 
     pwys <- sapply(unique(cluster.out$membership), FUN = function(i) {
       x <- cluster.out$names[cluster.out$membership == i]
@@ -34,7 +36,7 @@ make_gsea_cnet <- function(gseaRes, fc) {
     set.seed(100)
     plt <- enrichplot::cnetplot(gsea2, categorySize = "geneNum",  showCategory = length(pwys), node_label = "category",
                     color.params = list(foldChange = fc[[nm]], edge = TRUE, category = "orange"), cex.params = list(category_node = 1.5))
-    plt <- plt + ggplot2::scale_colour_gradient2(name = "Z-scores", low = "blue", mid = "white", high = "red") + ggplot2::ggtitle(nm)
+    plt <- plt + ggplot2::scale_colour_gradient2(name = label, low = "blue", mid = "white", high = "red") + ggplot2::ggtitle(nm)
     graphics::plot(plt)
 
     pltList[[nm]] <- plt
