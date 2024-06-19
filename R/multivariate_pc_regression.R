7b879a31ec5521b91c388dc83f65c2e0969d21de
Pan <PanH@joslin.harvard.edu>
2024-06-19 11:14:26 -0400

    add check to multivariate_pc_regression

--- R/multivariate_pc_regression.R
+++ R/multivariate_pc_regression.R
@@ -9,6 +9,10 @@
 multivariate_pc_regression <- function(object, pheno.df, metavars, num.pc=10, plot=TRUE, name="assoc_with_pc_pvals", width=8, height=7, xrot=0, yrot=0) {
   stopifnot(limma::isNumeric(object), nrow(object[rowSums(is.na(object))==0,]) > 0, ncol(object) > 0,
             !is.null(colnames(object)), is.logical(plot), colnames(object)==rownames(pheno.df), metavars %in% colnames(pheno.df))
+  fm <- stats::as.formula(paste0("~", paste(metavars, collapse="+")))
+  des <- stats::model.matrix(fm, pheno.df)
+  if (ncol(des) > ncol(object)) {
+    stop("Too many covariates")}
 
   pca <- stats::prcomp(t(object[rowSums(is.na(object))==0,]), scale.=FALSE)
   num.pc <- min(num.pc, ncol(object))
@@ -19,8 +23,8 @@ multivariate_pc_regression <- function(object, pheno.df, metavars, num.pc=10, pl
     fm <- stats::as.formula(paste0(pc, "~", paste(metavars, collapse="+")))
     lmfit <- stats::lm(fm, data = dat)
     av <- stats::aov(lmfit)
-    avRes <- as.data.frame(summary(av)[[1]]) %>%
-      .[metavars, ] %>%
+    avRes <- as.data.frame(summary(av)[[1]])
+    avRes <- avRes[metavars, ] %>%
       dplyr::pull(`Pr(>F)`)
   })
   colnames(pval) <- pcs
