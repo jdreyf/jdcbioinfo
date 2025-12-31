66e8861bc874e145b7ef1584ba63f60787174c48
jonathan dreyfuss <jdreyf@gmail.com>
2025-12-30 21:52:21 -0500

    fix add_avg_beta base

--- tests/testthat/test-add_avg_beta.R
+++ tests/testthat/test-add_avg_beta.R
@@ -1,5 +1,6 @@
 test_that("add_avg_beta", {
   expect_silent(mtt.aab <- add_avg_beta(mtt))
+  expect_equal(mtt.aab[1,1], 2^mtt[1,1]/(1 + 2^mtt[1,1])) # don't want to import sesame
   expect_error(add_avg_beta(mtt.aab))
   expect_gt(min(mtt.aab |> dplyr::select(tidyselect::ends_with("beta.avg"))), 0)
 })
