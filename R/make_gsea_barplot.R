a7ad454d3486a6891ee4d863a9feca228f342abf
Hui Pan <huipan1973@users.noreply.github.com>
2026-05-27 16:42:24 -0400

    update make_gsea_barplot

--- R/make_gsea_barplot.R
+++ R/make_gsea_barplot.R
@@ -5,10 +5,11 @@
 #' @param gseaObj GSEA object
 #' @param title plot title
 #' @param ntop number of up and down top pathways to shown
+#' @param plot whether to plot the barplot
 #' @return ggplot2 object.
 #' @export
 
-make_gsea_barplot <- function(gseaObj, title = "GSEA", ntop = 10) {
+make_gsea_barplot <- function(gseaObj, title = "GSEA", ntop = 10, plot = TRUE) {
   ggp <- gseaObj@result %>%
     dplyr::mutate(Direction = ifelse(NES > 0, "Positive", "Negative")) %>%
     dplyr::group_by(Direction) %>%
@@ -19,7 +20,10 @@ make_gsea_barplot <- function(gseaObj, title = "GSEA", ntop = 10) {
     ggplot2::ggplot(mapping = ggplot2::aes(x = Description, y = NES, fill = Direction)) + ggplot2::geom_col() + ggplot2::coord_flip()
   ggp <- ggp + ggplot2::ggtitle(title) + ggplot2::scale_fill_manual(values = c(Negative = "blue", Positive = "red"))
   ggp <- ggp + ggplot2::scale_x_discrete(labels = scales::label_wrap(60))
-  graphics::plot(ggp)
+
+  if (plot) {
+    graphics::plot(ggp)
+  }
 
   return(invisible(ggp))
 }
